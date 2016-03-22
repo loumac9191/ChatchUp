5c70d1c252cbefc08e41f89b1376bde831a63dca
loumac9191 <loumac91@hotmail.co.uk>
2016-03-22 11:29:02 +0000

    UserListBinding

--- TestClient/ViewModels/ChatPageViewModel.cs
+++ TestClient/ViewModels/ChatPageViewModel.cs
@@ -5,21 +5,36 @@ using System.ServiceModel;
 using System.Text;
 using ChatInterfaces;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
 
 namespace TestClient.ViewModels
 {
     public class ChatPageViewModel : BaseViewModel
     {
+        DuplexChannelFactory<IChatService> channelFactory;
+        List<string> usersLoggedIn; 
+
+
         public ChatPageViewModel()
         {
             //var chatClientImpl = new ChatClientImpl();
             //var channelFactory = new DuplexChannelFactory<IChatService>(chatClientImpl, "ChatServiceEndpoint");
-            var channelFactory = new DuplexChannelFactory<IChatService>(new ChatClientImpl(), "ChatServiceEndpoint");
+            channelFactory = new DuplexChannelFactory<IChatService>(new ChatClientImpl(), "ChatServiceEndpoint");
             var server = channelFactory.CreateChannel();
 
             User = Environment.UserName.ToString(); 
             server.Login(User);
+
+            usersLoggedIn = server.UsernameInChat();
+            userList = new ObservableCollection<string>(usersLoggedIn);
+
         }
+
+        //UPDATE
+        //private void UpdateCommands()
+        //{
+        //    ((Command)itemToRemoveFromDatabase).RaiseCanExecuteChanged();
+        //}
 
         private string _User;
 
@@ -28,13 +43,12 @@ namespace TestClient.ViewModels
             get { return _User; }
             set
             {
-                //redundant code?
-                _User = Environment.UserName.ToString();
+                _User = value;
                 OnPropertyChanged("User");
-                //_User = value;
             }
         }
 
+        //SEND BUTTON
         private ICommand _sendButton;
 
         public ICommand sendButton
@@ -52,13 +66,56 @@ namespace TestClient.ViewModels
 
         public void SendButton()
         {
-
+            //METHOD FOR SENDING MESSAGES
         }
 
         public bool CanPressSendButton()
         {
             return true;
         }
+
+        //SEND MESSAGE IN TEXTBOX
+        private string _messageToSend;
+
+        public string messageToSend
+        {
+            get { return _messageToSend; }
+            set 
+            {
+                _messageToSend = value;
+                OnPropertyChanged("messageToSend");
+            }
+        }
+
+        //MESSAGES THAT APPEAR IN THE MAIN LISTBOX
+        //UNFINISHED
+        private string _text;
+
+        public string text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+        
+        //LIST OF USERS
+        private ObservableCollection<string> _userList;
+
+        public ObservableCollection<string> userList
+        {
+            get { return _userList; }
+            set 
+            {
+                _userList = value;
+                OnPropertyChanged("userList");
+            }
+        }
+
+        //METHOD FOR GETTING USER LIST  
+        public void AllUsersCurrentlyLoggedIn()
+        {
+            //userList = server.UsernameInChat();
+        }
+        
         
     }
 }
